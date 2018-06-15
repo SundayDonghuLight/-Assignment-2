0747b729bf052f0f3e0c51b14799104088782ad7
SundayDonghuLight <38806778+SundayDonghuLight@users.noreply.github.com>
2018-06-15 19:14:09 +0800

    ver1.0
    辨識率慘不忍睹，幾乎都被辨識成數字1
    可能有overfiting的狀況

---
+++ MNIST_Recognition.py
@@ -0,0 +1,24 @@
+import numpy as np
+import random
+import matplotlib.pyplot as plt
+from sklearn.decomposition import PCA
+from sklearn.svm import SVC
+from sklearn.datasets import fetch_mldata
+mnist = fetch_mldata('MNIST original') 
+
+index = np.arange(len(mnist.data))
+random.shuffle(index)
+train_index = index[0:60000]
+test_index = index[60000:70000]
+X_train, y_train = mnist.data[train_index], mnist.target[train_index]
+X_test, y_test = mnist.data[test_index], mnist.target[test_index]
+
+pca = PCA(n_components=60)
+newX_train = pca.fit_transform(X_train)
+newX_test = pca.transform(X_test)
+
+clf = SVC()
+clf.fit(newX_train, y_train)
+print(np.mean(clf.predict(newX_train) == y_train))
+print(np.mean(clf.predict(newX_test) == y_test))
+ 
