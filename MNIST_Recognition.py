a49435653729aeff706d396ae63660f00146c6ab
SundayDonghuLight <38806778+SundayDonghuLight@users.noreply.github.com>
2018-06-15 19:54:50 +0800

    ver2.0

--- MNIST_Recognition.py
+++ MNIST_Recognition.py
@@ -2,7 +2,7 @@ import numpy as np
 import random
 import matplotlib.pyplot as plt
 from sklearn.decomposition import PCA
-from sklearn.svm import SVC
+from sklearn.neural_network import MLPClassifier
 from sklearn.datasets import fetch_mldata
 mnist = fetch_mldata('MNIST original') 
 
@@ -17,8 +17,7 @@ pca = PCA(n_components=60)
 newX_train = pca.fit_transform(X_train)
 newX_test = pca.transform(X_test)
 
-clf = SVC()
+clf = MLPClassifier(hidden_layer_sizes=(5))
 clf.fit(newX_train, y_train)
 print(np.mean(clf.predict(newX_train) == y_train))
 print(np.mean(clf.predict(newX_test) == y_test))
- 
