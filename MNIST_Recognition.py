cb134582fb4f8fe66e50d6275a7caf957de199f8
SundayDonghuLight <38806778+SundayDonghuLight@users.noreply.github.com>
2018-07-01 14:25:33 +0800

    ver3.0
    訓練資料辨識率：98.87%
    測試資料辨識率：98.29%
    數據進行歸一化後SVM可以有很好的辨識效果

--- MNIST_Recognition.py
+++ MNIST_Recognition.py
@@ -2,7 +2,7 @@ import numpy as np
 import random
 import matplotlib.pyplot as plt
 from sklearn.decomposition import PCA
-from sklearn.neural_network import MLPClassifier
+from sklearn.svm import SVC
 from sklearn.datasets import fetch_mldata
 mnist = fetch_mldata('MNIST original') 
 
@@ -16,8 +16,10 @@ X_test, y_test = mnist.data[test_index], mnist.target[test_index]
 pca = PCA(n_components=60)
 newX_train = pca.fit_transform(X_train)
 newX_test = pca.transform(X_test)
+newX_train = newX_train/255
+newX_test = newX_test/255
 
-clf = MLPClassifier(hidden_layer_sizes=(60))
+clf = SVC()
 clf.fit(newX_train, y_train)
 print(np.mean(clf.predict(newX_train) == y_train))
 print(np.mean(clf.predict(newX_test) == y_test))
